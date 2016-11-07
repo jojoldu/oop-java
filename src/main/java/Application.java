474db054ac498c9c6f2c476e79537eb8b986be6f
jojoldu <jojoldu@gmail.com>
2016-11-07 17:04:47 +0900

    프로젝트 생성

---
+++ src/main/java/Application.java
@@ -0,0 +1,10 @@
+/**
+ * Created by jojoldu@gmail.com on 2016-11-07.
+ * Blog : http://jojoldu.tistory.com
+ * Github : http://github.com/jojoldu
+ */
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
