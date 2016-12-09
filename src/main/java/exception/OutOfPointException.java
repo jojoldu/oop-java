857acc0e5e44d542a5e41a3226695a58c20f9b45
jojoldu <jojoldu@gmail.com>
2016-12-09 17:15:01 +0900

    리팩토링#3 - 네이밍 수정 작업중!

---
+++ src/main/java/exception/OutOfPointException.java
@@ -0,0 +1,10 @@
+package exception;
+
+/**
+ * Created by jojoldu@gmail.com on 2016-12-09
+ * Blog : http://jojoldu.tistory.com
+ * Github : http://github.com/jojoldu
+ */
+public class OutOfPointException extends RuntimeException{
+
+}
