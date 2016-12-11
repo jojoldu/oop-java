1f423fc02b55ec8537cd2903fae212d248ecd35e
이동욱 <jojoldu@gmail.com>
2016-12-11 23:11:28 +0900

    [리팩토링#3] 계층분리 설계중

---
+++ src/main/java/Client.java
@@ -0,0 +1,14 @@
+/**
+ * Created by jojoldu@gmail.com on 2016-12-11.
+ * Blog : http://jojoldu.tistory.com
+ * Github : http://github.com/jojoldu
+ */
+public class Client {
+    public void print(String msg) {
+
+    }
+
+    public void input() {
+
+    }
+}
